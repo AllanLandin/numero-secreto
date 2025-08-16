341aec099e586b3959ed9d8f54627f23fd3db386
Allan <allan.landin25@gmail.com>
2025-08-16 07:37:14 -0300

    alterando chamada da função

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ let numeroLimite = 20;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 6;
 
+exibirMensagemInicial();
+
 function exibirTextoNaTela(tag, texto) {
-  // testando
-  // testando mais uma vez
   let campo = document.querySelector(tag);
   campo.innerHTML = texto;
   responsiveVoice.speak(texto, "Brazilian Portuguese Female", { rate: 1.2 });
@@ -15,8 +15,6 @@ function exibirMensagemInicial() {
   exibirTextoNaTela("h1", "Jogo do número secreto");
   exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
 }
-
-exibirMensagemInicial();
 
 function verificarChute() {
   let chute = document.querySelector("input").value;
